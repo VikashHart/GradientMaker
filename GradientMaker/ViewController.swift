1c4f65e1b87cfdcaea9d8fbe12a7085e902a7f09
Vikash Hart <vikashhart@gmail.com>
2020-05-19 20:42:46 -0400

    Initial Commit

---
+++ GradientMaker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GradientMaker
+//
+//  Created by Vikash Hart on 5/19/20.
+//  Copyright © 2020 Vikash Hart. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
